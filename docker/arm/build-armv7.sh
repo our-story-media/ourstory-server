203cf1e923cf24aef0890ef30bf1465073dda466
Tom Bartindale <tom.bartindale@monash.edu>
2019-08-13 09:17:46 +1000

    combine arm builds to same file

--- docker/arm/build-armv7.sh
+++ docker/arm/build-armv7.sh
@@ -1,11 +1,11 @@
 echo "Building Server"
-docker buildx build --platform linux/arm/v7 -t bootlegger/ourstory-server:armv7 --push -f ../../Dockerfile.armv7  ../..
+docker buildx build --platform linux/arm/v7 -t bootlegger/ourstory-server:armv7 --push -f ../../Dockerfile.armv  ../..
 
 # echo "Building Proxy"
-# docker buildx build --platform linux/arm/v7 -t bootlegger/nginx-local:armv7 --push -f ../../Dockerfile.nginx.local ../../
+docker buildx build --platform linux/arm/v7 -t bootlegger/nginx-local:armv7 --push -f ../../Dockerfile.nginx.local ../../
 
 # echo "Building Beanstalkd"
-# docker buildx build --platform linux/arm/v7 -t bootlegger/beanstalk:armv7 --push -f Dockerfile.beanstalkd .
+docker buildx build --platform linux/arm/v7 -t bootlegger/beanstalk:armv7 --push -f Dockerfile.beanstalkd .
 
 # echo "Building Worker"
-# docker buildx build --platform linux/arm/v7 -t bootlegger/ourstory-worker:armv7 --push ../../../ourstory-worker/
+docker buildx build --platform linux/arm/v7 -t bootlegger/ourstory-worker:armv7 --push ../../../ourstory-worker/
