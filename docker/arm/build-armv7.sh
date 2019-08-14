5d825ead6694be52383073380f3bda85bd9e6385
Tom Bartindale <tom.bartindale@monash.edu>
2019-08-14 14:53:52 +1000

    only build nginx in arm

--- docker/arm/build-armv7.sh
+++ docker/arm/build-armv7.sh
@@ -1,11 +1,12 @@
 echo "Building Server"
-docker buildx build --platform linux/arm/v7 -t bootlegger/ourstory-server:armv7 --push -f ../../Dockerfile.armv  ../..
+# docker buildx build --platform linux/arm/v7 -t bootlegger/ourstory-server:armv7 --push -f ../../Dockerfile.armv  ../..
 
 # echo "Building Proxy"
-docker buildx build --platform linux/arm/v7 -t bootlegger/nginx-local:armv7 --push -f ../../Dockerfile.nginx.local ../../
+docker buildx build --no-cache --platform=linux/arm/v7,linux/arm64,linux/amd64 -t bootlegger/nginx --push -f ../../Dockerfile.nginx ../../
+docker buildx build --no-cache --platform=linux/arm/v7,linux/amd64,linux/arm64 -t bootlegger/nginx-local --push -f ../../Dockerfile.nginx.local ../../
 
 # echo "Building Beanstalkd"
-docker buildx build --platform linux/arm/v7 -t bootlegger/beanstalk:armv7 --push -f Dockerfile.beanstalkd .
+# docker buildx build --platform linux/arm/v7 -t bootlegger/beanstalk:armv7 --push -f Dockerfile.beanstalkd .
 
 # echo "Building Worker"
-docker buildx build --platform linux/arm/v7 -t bootlegger/ourstory-worker:armv7 --push ../../../ourstory-worker/
+# docker buildx build --platform linux/arm/v7 -t bootlegger/ourstory-worker:armv7 --push ../../../ourstory-worker/
