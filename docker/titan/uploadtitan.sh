66505776f98a2f1935bf08403e2b8fbdc2270feb
Tom Bartindale <tom.bartindale@monash.edu>
2019-11-04 15:58:04 +1100

    include version file

--- docker/titan/uploadtitan.sh
+++ docker/titan/uploadtitan.sh
@@ -2,11 +2,11 @@
 
 echo "Downloading latest tagged version"
 
-docker pull bootlegger/titan-compact:latest
+# docker pull bootlegger/titan-compact:latest
 
 echo "Saving to tar file"
 
-docker save -o indaba-update.tar bootlegger/titan-compact:latest
+# docker save -o indaba-update.tar bootlegger/titan-compact:latest
 
 echo "Get Versions"
 
@@ -14,18 +14,26 @@ VERSION=$(docker run bootlegger/titan-compact:latest bash -c 'cd /ourstory-serve
 
 VERSION=$(echo $VERSION | sed 's/ /-/g')
 
+echo "Version is $VERSION"
+
+echo $VERSION > indaba-update.version
+
 echo "Upload to S3"
 
 FILENAME=indaba-update-armv7-$VERSION.tar
 
 echo "Renaming to $FILENAME"
 
-mv indaba-update.tar $FILENAME
+# mv indaba-update.tar $FILENAME
 
 echo "Uploading $FILENAME"
 
-aws s3 cp $FILENAME s3://ourstory-v2-live/titan/
+# aws s3 cp $FILENAME s3://ourstory-v2-live/titan/
+
+echo "Uploading Version"
+
+aws s3 cp indaba-update.version s3://ourstory-v2-live/titan/
 
 echo "Copy to indaba-update.tar"
 
-aws s3 cp s3://ourstory-v2-live/titan/$FILENAME s3://ourstory-v2-live/titan/indaba-update.tar
+# aws s3 cp s3://ourstory-v2-live/titan/$FILENAME s3://ourstory-v2-live/titan/indaba-update.tar
