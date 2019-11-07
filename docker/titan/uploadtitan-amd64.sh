af2ec9e2a8c126bccf8d4389a1673e44f4678826
Tom Bartindale <tom.bartindale@monash.edu>
2019-11-07 21:00:46 +1100

    uploader

--- docker/titan/uploadtitan-amd64.sh
+++ docker/titan/uploadtitan-amd64.sh
@@ -2,15 +2,15 @@
 
 echo "Downloading latest tagged version"
 
-# docker pull redis:alpine
-# docker pull mvertes/alpine-mongo
-# docker pull kusmierz/beanstalkd
-# docker pull bootlegger/ourstory-worker:latest
-# docker pull bootlegger/ourstory-server:latest
-# docker pull bootlegger/nginx-local:latest
+docker pull redis:alpine
+docker pull mvertes/alpine-mongo
+docker pull kusmierz/beanstalkd
+docker pull bootlegger/ourstory-worker:latest
+docker pull bootlegger/ourstory-server:latest
+docker pull bootlegger/nginx-local:latest
 
 # echo "Exporting Images"
-# docker save redis:alpine mvertes/alpine-mongo kusmierz/beanstalkd bootlegger/nginx-local:latest bootlegger/ourstory-worker:latest bootlegger/ourstory-server:latest -o images.tar
+docker save redis:alpine mvertes/alpine-mongo kusmierz/beanstalkd bootlegger/nginx-local:latest bootlegger/ourstory-worker:latest bootlegger/ourstory-server:latest -o images.tar
 
 echo "Get Versions"
 
