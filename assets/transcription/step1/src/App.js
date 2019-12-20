91d1c0c79c4eb54c845f11f6294c59232c12fae7
Peter.Chen1 <peter.chen@monash.edu>
2019-12-20 13:25:56 +1100

    fix for times

--- assets/transcription/step1/src/App.js
+++ assets/transcription/step1/src/App.js
@@ -1,25 +1,31 @@
 import React from "react";
 import "./App.css";
 import VideoPlayer from "./components/videoPlayer";
-import CssBaseline from '@material-ui/core/CssBaseline';
-import Typography from '@material-ui/core/Typography';
-import Container from '@material-ui/core/Container';
+import CssBaseline from "@material-ui/core/CssBaseline";
+import Typography from "@material-ui/core/Typography";
+import Container from "@material-ui/core/Container";
 
 function App() {
+  var id = window.location.href.split("/")[5].split("?")[0];
+  var apikey = window.location.href.split("apikey=")[1];
 
-  // var id = window.location.href.split('/')[5].split('?')[0];
-  // var apikey = window.location.href.split('apikey=')[1];
-
-  var id = 1;
-  var apikey = "";
+  // var id = 1;
+  // var apikey = "";
 
   return (
     <main>
       <Container maxWidth="lg">
-      <CssBaseline />
-      {/* <VideoPlayer src={'/api/watch/getvideo/'+id} id={id} apikey={apikey}></VideoPlayer> */}
-      <VideoPlayer src={'http://media.w3.org/2010/05/bunny/movie.mp4'} id={id} apikey={apikey}></VideoPlayer>
-
+        <CssBaseline />
+        <VideoPlayer
+          src={"/api/watch/getvideo/" + id}
+          id={id}
+          apikey={apikey}
+        ></VideoPlayer>
+        {/* <VideoPlayer
+          src={"http://media.w3.org/2010/05/bunny/movie.mp4"}
+          id={id}
+          apikey={apikey}
+        ></VideoPlayer> */}
       </Container>
     </main>
   );
--- assets/transcription/step1/src/components/videoPlayer.jsx
+++ assets/transcription/step1/src/components/videoPlayer.jsx
@@ -2,24 +2,23 @@ import React, { Component } from "react";
 import { Player, BigPlayButton, ControlBar, Shortcut } from "video-react";
 import SingleLineGridList from "./lineGrid";
 import PopupDialog from "./popupDialog";
-import axios from 'axios';
-import {Button,Container} from '@material-ui/core';
-import Fab from '@material-ui/core/Fab';
-import AddIcon from '@material-ui/icons/Add';
-import Replay5 from '@material-ui/icons/Replay5';
-import Snackbar from '@material-ui/core/Snackbar';
-import ReactDOM from 'react-dom'
+import axios from "axios";
+import { Button, Container } from "@material-ui/core";
+import Fab from "@material-ui/core/Fab";
+import AddIcon from "@material-ui/icons/Add";
+import Replay5 from "@material-ui/icons/Replay5";
+import Snackbar from "@material-ui/core/Snackbar";
+import ReactDOM from "react-dom";
 import { ArrowBack } from "@material-ui/icons";
 
 export default class VideoPlayer extends Component {
-  
   constructor(props, context) {
     super(props, context);
 
     this.state = {
       original: {
-        transcription:{
-          chunks:[]
+        transcription: {
+          chunks: []
         }
       },
       source: props.src,
@@ -27,36 +26,35 @@ export default class VideoPlayer extends Component {
       options: ["Backward 2 Seconds", "Add New Breakpoint", "Discard Setting"],
       erroropen: false
     };
-
-    
   }
 
   componentDidMount() {
     // Subscribe state change
     this.player.subscribeToStateChange(this.handleStateChange.bind(this));
 
-      this.serverRequest = axios.get('/api/watch/edit/'+this.props.id).then(function (result) {
+    this.serverRequest = axios.get("/api/watch/edit/" + this.props.id).then(
+      function(result) {
         // var lastGist = result[0];
         // console.log(result);
 
-        if (!result.data.transcription)
-        {
+        if (!result.data.transcription) {
           result.data.transcription = {
-            chunks:[]
-          }
+            chunks: []
+          };
         }
 
         result.data.transcription.chunks.forEach(chunk => {
           chunk.starttime = toMS(chunk.starttime);
           chunk.endtime = toMS(chunk.endtime);
-        })
+        });
 
         console.log(result.data);
 
         this.setState({
           original: result.data
         });
-      }.bind(this));
+      }.bind(this)
+    );
   }
 
   handleStateChange(state) {
@@ -67,31 +65,25 @@ export default class VideoPlayer extends Component {
   }
 
   saveEdit() {
-    // let { original } = this.state;
-    // let newChunks = [...this.state.original.transcription.chunks];
+    let newTranscription = Object.assign({}, this.state.original.transcription);
+    let newChunks = [];
 
-    // let newOriginal = {transcription: {}}
-    let newOriginal = Object.assign({}, this.state.original);
-    let newChunks = [...newOriginal.transcription.chunks]
-
-    // let newTranscription = Array.assign([],this.state.original.trq);
-
-    console.log(newChunks);
-    // newChunks.forEach(chunk => {
-    for (var i=0;i<newChunks.length;i++)
-    {
-      newChunks[i].starttime1 =  toSrt({...newChunks[i]}.starttime);
-      newChunks[i].endtime1 = toSrt({...newChunks[i]}.endtime);
+    const chunks = newTranscription.chunks;
+    for (var i = 0; i < chunks.length; i++) {
+      let newChunk = {};
+      newChunk.starttime = toSrt({ ...chunks[i] }.starttime);
+      newChunk.endtime = toSrt({ ...chunks[i] }.endtime);
+      newChunks.push(newChunk);
     }
-    
+
     console.log("newchunks: ", newChunks);
     // newOriginal.transcription.chunks = newChunks;
 
-    // axios.post('/api/watch/savedit/' + this.props.id + '?apikey=' + this.props.apikey,  newOriginal).then(function (result) { 
+    // axios.post('/api/watch/savedit/' + this.props.id + '?apikey=' + this.props.apikey,  newOriginal).then(function (result) {
     //   // Update last saved display
     //   console.log(result)
     // });
-  };
+  }
 
   handlePlay = starttime => {
     this.player.seek(starttime);
@@ -101,23 +93,24 @@ export default class VideoPlayer extends Component {
   skipBack = () => {
     const { player } = this.player.getState();
     this.player.seek(player.currentTime - 2);
-  }
+  };
 
   backButton = () => {
     window.history.back();
-  }
+  };
 
   handleDelete = (starttime, endtime) => {
-
     // Remove the chunk selected
     let original = this.state.original;
-    let originalchunks = original.transcription.chunks.filter(c => c.starttime !== starttime);
+    let originalchunks = original.transcription.chunks.filter(
+      c => c.starttime !== starttime
+    );
 
     // Replace the next chunk's start time with the start time of the chunk deleted
-    let index = this.state.original.transcription.chunks.findIndex(c => c.starttime === endtime);
+    let index = this.state.original.transcription.chunks.findIndex(
+      c => c.starttime === endtime
+    );
     if (index >= 1) originalchunks[--index].starttime = starttime;
-
-    // console.log(original)
 
     original.transcription.chunks = originalchunks;
 
@@ -152,7 +145,7 @@ export default class VideoPlayer extends Component {
   handleConfirm() {
     // Record the thumbnail of the current endtime
     const { currentTime } = this.state.player;
-    let original = Object.assign({},this.state.original);
+    let original = Object.assign({}, this.state.original);
 
     let newChunk = {
       endtime: currentTime
@@ -163,8 +156,7 @@ export default class VideoPlayer extends Component {
       c => currentTime >= c.starttime && currentTime <= c.endtime
     );
 
-    if (currentTime == 0)
-    {
+    if (currentTime == 0) {
       this.setState({
         erroropen: true
       });
@@ -189,131 +181,135 @@ export default class VideoPlayer extends Component {
       original.transcription.chunks.push(newChunk);
     }
 
-    // console.log(original)
-    
-    this.setState({original},function(){
-      console.log("Just before save", original);
-      // console.log(this.state.original);
-      
-      this.saveEdit();
-    });
+    console.log("Original: ", original);
 
+    this.setState({ original });
+    this.saveEdit();
   }
 
-  closeError = ()=>{
+  closeError = () => {
     this.setState({
       erroropen: false
     });
-  }
+  };
 
   render() {
     return (
       <div>
         <br />
-        {/* <Card>
-          <CardContent>
-          
-          </CardContent>
-        </Card>
-        <br /> */}
-        <h2 style={{position:'absolute', width:'15%', right:'1em', top:'3em'}}>Create a breakpoint when someone stops talking</h2>
-        <Container style={{maxWidth:'800px'}}>
-
-        <Player
-          ref={player => {
-            this.player = player;
+        <h2
+          style={{
+            position: "absolute",
+            width: "15%",
+            right: "1em",
+            top: "3em"
           }}
+        >
+          Create a breakpoint when someone stops talking
+        </h2>
+        <Container style={{ maxWidth: "800px" }}>
+          <Player
+            ref={player => {
+              this.player = player;
+            }}
           >
-          <source src={this.state.source} />
-          <BigPlayButton position="center" />
-          <Shortcut clickable={true} />
-          <ControlBar autoHide={false} />
-        </Player>
+            <source src={this.state.source} />
+            <BigPlayButton position="center" />
+            <Shortcut clickable={true} />
+            <ControlBar autoHide={false} />
+          </Player>
         </Container>
-<br />
+        <br />
         {/* <div className="py-3"> */}
-          {/* <Button  className="m-2">
+        {/* <Button  className="m-2">
             Set Breakpoint
           </Button> */}
 
-          <SingleLineGridList
-          style={{width:'100%'}}
-            onPlay={this.handlePlay}
-            onDelete={this.handleDelete}
-            chunks={this.state.original.transcription.chunks}
-          ></SingleLineGridList>
+        <SingleLineGridList
+          style={{ width: "100%" }}
+          onPlay={this.handlePlay}
+          onDelete={this.handleDelete}
+          chunks={this.state.original.transcription.chunks}
+        ></SingleLineGridList>
 
-          <PopupDialog
-            open={this.state.open}
-            options={this.state.options}
-            onClose={this.handleClose}
-          />
+        <PopupDialog
+          open={this.state.open}
+          options={this.state.options}
+          onClose={this.handleClose}
+        />
 
-          <div id="output"></div>
-          {/* <div>{JSON.stringify(this.state.original)}</div> */}
-          <Fab color="primary" onClick={this.skipBack} aria-label="left" style={{position:'absolute', left:'2em', top:'45%'}}>
-            <Replay5 />
-          </Fab>
-          <Fab color="primary" onClick={this.handleClickOpen} aria-label="add" style={{position:'absolute', right:'1em', top:'45%'}}>
-            <AddIcon />
-          </Fab>
-          <Button onClick={this.backButton} style={{position:'absolute', left:'0', top:'1em'}}>
-              <ArrowBack />
-            </Button>
-          <Snackbar
-        anchorOrigin={{
-          vertical: 'bottom',
-          horizontal: 'center',
-        }}
-        open={this.state.erroropen}
-        autoHideDuration={1000}
-        onClose={this.closeError}
-        message="Cannot add breakpoint here"
-      ></Snackbar>
-        </div>
+        <div id="output"></div>
+        {/* <div>{JSON.stringify(this.state.original)}</div> */}
+        <Fab
+          color="primary"
+          onClick={this.skipBack}
+          aria-label="left"
+          style={{ position: "absolute", left: "2em", top: "45%" }}
+        >
+          <Replay5 />
+        </Fab>
+        <Fab
+          color="primary"
+          onClick={this.handleClickOpen}
+          aria-label="add"
+          style={{ position: "absolute", right: "1em", top: "45%" }}
+        >
+          <AddIcon />
+        </Fab>
+        <Button
+          onClick={this.backButton}
+          style={{ position: "absolute", left: "0", top: "1em" }}
+        >
+          <ArrowBack />
+        </Button>
+        <Snackbar
+          anchorOrigin={{
+            vertical: "bottom",
+            horizontal: "center"
+          }}
+          open={this.state.erroropen}
+          autoHideDuration={1000}
+          onClose={this.closeError}
+          message="Cannot add breakpoint here"
+        ></Snackbar>
+      </div>
       // </div>
     );
   }
 }
 
-function toMS (timestamp) {
-
-  return 1234;
-
-  console.log("timestamp : " +timestamp);
-
+function toMS(timestamp) {
   if (!isNaN(timestamp)) {
-    return timestamp
+    return timestamp;
   }
 
-  const match = timestamp.match(/^(?:(\d{2,}):)?(\d{2}):(\d{2})[,.](\d{3})$/)
+  const match = timestamp.match(/^(?:(\d{2,}):)?(\d{2}):(\d{2})[,.](\d{3})$/);
 
   if (!match) {
-    throw new Error('Invalid SRT or VTT time format: "' + timestamp + '"')
+    throw new Error('Invalid SRT or VTT time format: "' + timestamp + '"');
   }
 
-  const hours = match[1] ? parseInt(match[1], 10) * 3600000 : 0
-  const minutes = parseInt(match[2], 10) * 60000
-  const seconds = parseInt(match[3], 10) * 1000
-  const milliseconds = parseInt(match[4], 10)
+  const hours = match[1] ? parseInt(match[1], 10) * 3600000 : 0;
+  const minutes = parseInt(match[2], 10) * 60000;
+  const seconds = parseInt(match[3], 10) * 1000;
+  const milliseconds = parseInt(match[4], 10);
 
-  return hours + minutes + seconds + milliseconds
+  return hours + minutes + seconds + milliseconds;
 }
 
-function toSrt (duration) {
+function toSrt(duration) {
   // return "11:11:11,000";
-  
-  console.log("duration: " + duration)
+
+  console.log("duration: " + duration);
   let timestamp = duration * 1000;
   var milliseconds = parseInt((parseFloat(timestamp) % 1000) / 100),
-  seconds = Math.floor((timestamp / 1000) % 60),
-  minutes = Math.floor((timestamp / (1000 * 60)) % 60),
-  hours = Math.floor((timestamp / (1000 * 60 * 60)) % 24);
+    seconds = Math.floor((timestamp / 1000) % 60),
+    minutes = Math.floor((timestamp / (1000 * 60)) % 60),
+    hours = Math.floor((timestamp / (1000 * 60 * 60)) % 24);
 
-  
-  hours = (hours < 10) ? "0" + hours : hours;
-  minutes = (minutes < 10) ? "0" + minutes : minutes;
-  seconds = (seconds < 10) ? "0" + seconds : seconds;
+  hours = hours < 10 ? "0" + hours : hours;
+  minutes = minutes < 10 ? "0" + minutes : minutes;
+  seconds = seconds < 10 ? "0" + seconds : seconds;
 
   return hours + ":" + minutes + ":" + seconds + "," + milliseconds;
 }
