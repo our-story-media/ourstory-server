dd210760245a389418ea5d37ad425a6bc7c5c64d
Tom Bartindale <tom.bartindale@monash.edu>
2024-01-12 15:10:39 +0000

    fix docs static build

--- docker/builddocs.js
+++ docker/builddocs.js
@@ -4,11 +4,13 @@ const fs = require("fs");
 // config.base = "/docs/";
 
 let config = fs.readFileSync(
-  "../ourstory-resources/doc_gen/.vuepress/config.js"
+  "./ourstory-resources/doc_gen/.vuepress/config.js"
 );
 
 config = config.toString().replace('base: "/"', 'base: "/docs/"');
 
-fs.writeFileSync("../ourstory-resources/doc_gen/.vuepress/config.js", config);
+// console.log(config);
+
+fs.writeFileSync("./ourstory-resources/doc_gen/.vuepress/config.js", config);
 
 console.log("Config file updated");
