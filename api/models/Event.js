cfb86f3416279eec373dff0b4279499494535061
Tom Bartindale <tom.bartindale@monash.edu>
2024-01-24 14:24:13 +0000

    mkdir with chmod on event create

--- api/models/Event.js
+++ api/models/Event.js
@@ -3,7 +3,7 @@
  * This software may be modified and distributed under the terms
  * of the MIT license. See the LICENSE file for details.
  */
- /**
+/**
  * Event
  *
  * @module      :: Model
@@ -12,305 +12,272 @@
  */
 
 var JSZip = require("jszip");
-var path = require('path');
-var fs = require('fs');
-var dir = __dirname + '../../../data/';	
-var moment = require('moment');
+var path = require("path");
+var fs = require("fs");
+var dir = __dirname + "../../../data/";
+var moment = require("moment");
 
 module.exports = {
-
- attributes: {
-	name: {
-	    type: 'string',
-	    required:true,
-	    minLength: 5
-	  },
-	starts: {
-	    required:true
-	  },
+  attributes: {
+    name: {
+      type: "string",
+      required: true,
+      minLength: 5,
+    },
+    starts: {
+      required: true,
+    },
     ends: {
-	    required:true
-	  },
-	  eventtype: {
-	    type: 'json'
-	  },
+      required: true,
+    },
+    eventtype: {
+      type: "json",
+    },
 
-	  calcphases:function()
-	  {
-	  	//console.log(this.starts);
-	  	//console.log(moment(this.starts));
+    calcphases: function () {
+      //console.log(this.starts);
+      //console.log(moment(this.starts));
 
-	  	if (this.commissiondone)
-	  		this.iscommission = true;
+      if (this.commissiondone) this.iscommission = true;
 
-	  	if (this.preparedone)
-	  		this.isprepare = true;
+      if (this.preparedone) this.isprepare = true;
 
-	  	var starts = moment(this.starts,"DD-MM-YYYY");
-	  	if (starts < moment())
-	  	{
-	  		this.iscommission = true;
-	  		this.isprepare = true;
-	  		this.isshoot = true;
-	  	}
-	
-		//console.log(moment(this.ends,"DD-MM-YYYY"));
-	  	var ends = moment(this.ends,"DD-MM-YYYY");
-	  	if (ends < moment())
-	  	{
-	  		this.iscommission = true;
-	  		this.isprepare = true;
-	  		this.isshoot = true;
-	  		this.ispost = true;
-	  	}
+      var starts = moment(this.starts, "DD-MM-YYYY");
+      if (starts < moment()) {
+        this.iscommission = true;
+        this.isprepare = true;
+        this.isshoot = true;
+      }
 
+      //console.log(moment(this.ends,"DD-MM-YYYY"));
+      var ends = moment(this.ends, "DD-MM-YYYY");
+      if (ends < moment()) {
+        this.iscommission = true;
+        this.isprepare = true;
+        this.isshoot = true;
+        this.ispost = true;
+      }
+    },
 
-	  },
-	  
-	  zipexists:function()
-	  {
-		dir = path.normalize(dir);
-		// console.log("checking zip at "+dir+'/'+this.id+'.zip');
-		// console.log("zip " + fs.existsSync(dir+'/'+this.id+'.zip'));
-		return fs.existsSync(dir+'/'+this.id+'.zip');
-	  },
+    zipexists: function () {
+      dir = path.normalize(dir);
+      // console.log("checking zip at "+dir+'/'+this.id+'.zip');
+      // console.log("zip " + fs.existsSync(dir+'/'+this.id+'.zip'));
+      return fs.existsSync(dir + "/" + this.id + ".zip");
+    },
 
-	  genzip:function(cb)
-	  {
-		//   console.log("genzip " + this.id);
-			dir = path.normalize(dir);
-			// console.log(dir);
-			var images = _.pluck(this.eventtype.shot_types,'image');
-			var icons = _.pluck(this.eventtype.shot_types,'icon');
+    genzip: function (cb) {
+      //   console.log("genzip " + this.id);
+      dir = path.normalize(dir);
+      // console.log(dir);
+      var images = _.pluck(this.eventtype.shot_types, "image");
+      var icons = _.pluck(this.eventtype.shot_types, "icon");
 
-			var im_files = _.map(images,function(m)
-			{
-				return {source:dir+'images/'+m,target:m};
-			});
+      var im_files = _.map(images, function (m) {
+        return { source: dir + "images/" + m, target: m };
+      });
 
-			var ic_files = _.map(icons,function(m){
-				return {source:dir+'icons/'+m,target:'icons/'+m}
-			});
+      var ic_files = _.map(icons, function (m) {
+        return { source: dir + "icons/" + m, target: "icons/" + m };
+      });
 
-			var all = _.union(im_files, ic_files);
+      var all = _.union(im_files, ic_files);
 
-			var zip = new JSZip();
-			_.each(all,function(f)
-			{
-				if (fs.existsSync(f.source))
-				{
-					try
-					{
-						zip.file(f.target, fs.readFileSync(f.source));
-					}
-					catch (e)
-					{
-						console.log(e);
-					}
-				}
-				else
-				{
-					//missing file...
-					sails.log.verbose('missing file: '+f.source);
-				}
-			});
+      var zip = new JSZip();
+      _.each(all, function (f) {
+        if (fs.existsSync(f.source)) {
+          try {
+            zip.file(f.target, fs.readFileSync(f.source));
+          } catch (e) {
+            console.log(e);
+          }
+        } else {
+          //missing file...
+          sails.log.verbose("missing file: " + f.source);
+        }
+      });
 
-			try
-			{
-				var outputfilename = dir + this.id + '.zip';
-				zip.generateAsync({ type: "nodebuffer", compression: 'DEFLATE' })
-				.then(function (content) {
-					// use content
-					// console.log('writing '+outputfilename);
-					require("fs").writeFile(outputfilename, content, function (err) {
-						if (err)
-							console.log(err);
-						cb();
-					});
-				});
+      try {
+        var outputfilename = dir + this.id + ".zip";
+        zip
+          .generateAsync({ type: "nodebuffer", compression: "DEFLATE" })
+          .then(function (content) {
+            // use content
+            // console.log('writing '+outputfilename);
+            require("fs").writeFile(outputfilename, content, function (err) {
+              if (err) console.log(err);
+              cb();
+            });
+          });
 
-				// require("fs").writeFile(dir+'/'+this.id+'.zip', content, function(err){
-				// cb();
-				// });
-			}
-			catch (e)
-			{
-				console.log(e);
-				console.log('no file written');
-				cb();
-			}
-			
-	  }
-
-    
+        // require("fs").writeFile(dir+'/'+this.id+'.zip', content, function(err){
+        // cb();
+        // });
+      } catch (e) {
+        console.log(e);
+        console.log("no file written");
+        cb();
+      }
+    },
   },
 
   beforeUpdate: function (valuesToSet, proceed) {
-	  valuesToSet.apikey = null;
-      return proceed();
+    valuesToSet.apikey = null;
+    return proceed();
   },
 
-  afterCreate:function(newlyInsertedRecord, cb)
-  {
-  	Log.logModel('Event',newlyInsertedRecord.id);
-  	cb();
+  afterCreate: function (newlyInsertedRecord, cb) {
+    Log.logModel("Event", newlyInsertedRecord.id);
+    //make directory in upload dir (in case this is done before nginx upload):
+    let upload_path = path.join(
+      __dirname,
+      "..",
+      "..",
+      "upload",
+      newlyInsertedRecord.id
+    );
+
+    fs.mkdirSync(upload_path);
+    fs.chmodSync(upload_path, 0o777);
+    cb();
   },
 
-  findByListBuildVariant:function(req,events,cb)
-  {
-	  var currentbv = req.session.buildvariant;
-	  if (currentbv.search_user)
-	  {
-		  Event.find({
-			  id:events,
-			  'ownedby': 
-				{
-					contains: currentbv.search_user
-				}
-				}).exec(cb);
-	  }
-	  else
-	  {
-		 Event.find(events).exec(cb);
-	  }
+  findByListBuildVariant: function (req, events, cb) {
+    var currentbv = req.session.buildvariant;
+    if (currentbv.search_user) {
+      Event.find({
+        id: events,
+        ownedby: {
+          contains: currentbv.search_user,
+        },
+      }).exec(cb);
+    } else {
+      Event.find(events).exec(cb);
+    }
   },
 
-  findViaBuildVariant:function(req,cb){
-	  var currentbv = req.session.buildvariant;
-	  if (currentbv.search_user)
-	  {
-		  Event.find().where({
-								public:[1,true],
-								'ownedby': 
-								{
-									contains: currentbv.search_user
-								}
-							}).exec(cb);
-	  }
-	  else
-	  {
-	  	Event.find().where({public:[1,true]}).exec(cb);
-	  }
+  findViaBuildVariant: function (req, cb) {
+    var currentbv = req.session.buildvariant;
+    if (currentbv.search_user) {
+      Event.find()
+        .where({
+          public: [1, true],
+          ownedby: {
+            contains: currentbv.search_user,
+          },
+        })
+        .exec(cb);
+    } else {
+      Event.find()
+        .where({ public: [1, true] })
+        .exec(cb);
+    }
   },
 
-  findOwnedByBuildVariant:function(req,cb)
-  {
-	if (req.session.passport && req.session.passport.user)
-	{
-	  var currentbv = req.session.buildvariant;
-	  if (currentbv.search_user)
-	  {
-		  cb(null,[]);
-	  }
-	  else
-	  {
-	  	Event.find().where({'ownedby': {
-			contains: req.session.passport.user.id
-		}}).exec(cb);
-	  }
-	}
-	else
-	{
-		cb(null,[]);
-	}
+  findOwnedByBuildVariant: function (req, cb) {
+    if (req.session.passport && req.session.passport.user) {
+      var currentbv = req.session.buildvariant;
+      if (currentbv.search_user) {
+        cb(null, []);
+      } else {
+        Event.find()
+          .where({
+            ownedby: {
+              contains: req.session.passport.user.id,
+            },
+          })
+          .exec(cb);
+      }
+    } else {
+      cb(null, []);
+    }
   },
 
-  findInvitedByBuildVariant:function(req,cb)
-  {
-	if (req.session.passport && req.session.passport.user)
-	{
-		var currentbv = req.session.buildvariant;
-		if (currentbv.search_user)
-		{
-			Event.find().where({
-					'codes.uid':req.session.passport.user.id,
-					'ownedby': 
-					{
-						contains: currentbv.search_user
-					}
-				}).exec(cb);
-		}
-		else
-		{
-			Event.find().where({'codes.uid':req.session.passport.user.id}).exec(cb);
-		}
-	}
-	else
-	{
-		cb(null,[]);
-	}
+  findInvitedByBuildVariant: function (req, cb) {
+    if (req.session.passport && req.session.passport.user) {
+      var currentbv = req.session.buildvariant;
+      if (currentbv.search_user) {
+        Event.find()
+          .where({
+            "codes.uid": req.session.passport.user.id,
+            ownedby: {
+              contains: currentbv.search_user,
+            },
+          })
+          .exec(cb);
+      } else {
+        Event.find()
+          .where({ "codes.uid": req.session.passport.user.id })
+          .exec(cb);
+      }
+    } else {
+      cb(null, []);
+    }
   },
 
-
-  findFeaturedByBuildVariant:function(req,cb)
-	{
-		var currentbv = req.session.buildvariant;
-		if (currentbv.search_user)
-		{
-			Event.find({where:{
-				public:[1,true],
-				'ownedby': 
-				{
-					contains: currentbv.search_user
-				}
-			},sort:'starts DESC',limit:12}).exec(cb);
-		}
-		else
-		{
-			
-			Event.find({where:{public:[1,true]},sort:'starts DESC',limit:12}).exec(cb);
-		}
-	},
-
-  findListByBuildVariant:function(req,events,cb)
-  {
-	  	var currentbv = req.session.buildvariant;
-		if (currentbv.search_user)
-		{
-			Event.find({where:{
-				id:events,
-				'ownedby': 
-				{
-					contains: currentbv.search_user
-				}
-			}}).exec(cb);
-		}
-		else
-		{
-			Event.find(events).exec(cb);
-		}
+  findFeaturedByBuildVariant: function (req, cb) {
+    var currentbv = req.session.buildvariant;
+    if (currentbv.search_user) {
+      Event.find({
+        where: {
+          public: [1, true],
+          ownedby: {
+            contains: currentbv.search_user,
+          },
+        },
+        sort: "starts DESC",
+        limit: 12,
+      }).exec(cb);
+    } else {
+      Event.find({
+        where: { public: [1, true] },
+        sort: "starts DESC",
+        limit: 12,
+      }).exec(cb);
+    }
   },
 
-  getnewcode:function(cb)
-  {
-  	//return a code unique across all events
-  	var done = false;
-  	var newcode = '';
-  	//find list of codes...
-  	Event.find().exec(function(err, events) {
-		// Do stuff here
-		var allcodes = new Array();
-
-		//get all codes:
-		_.each(events, function(ev)
-		{
-			_.each(ev.codes,function(c)
-			{
-				allcodes.push(c.code);
-			});		  	
-		});
-
-		while (!done)
-	  	{
-			newcode = Math.floor((Math.random()*99999)).toString().substring(0,4);
-			//find list of codes
-			if (!_.contains(allcodes,newcode))
-				done=true;
-		}
-		//console.log('newcode: '+newcode);
-		cb(newcode);
-	});
+  findListByBuildVariant: function (req, events, cb) {
+    var currentbv = req.session.buildvariant;
+    if (currentbv.search_user) {
+      Event.find({
+        where: {
+          id: events,
+          ownedby: {
+            contains: currentbv.search_user,
+          },
+        },
+      }).exec(cb);
+    } else {
+      Event.find(events).exec(cb);
+    }
   },
 
+  getnewcode: function (cb) {
+    //return a code unique across all events
+    var done = false;
+    var newcode = "";
+    //find list of codes...
+    Event.find().exec(function (err, events) {
+      // Do stuff here
+      var allcodes = new Array();
 
+      //get all codes:
+      _.each(events, function (ev) {
+        _.each(ev.codes, function (c) {
+          allcodes.push(c.code);
+        });
+      });
+
+      while (!done) {
+        newcode = Math.floor(Math.random() * 99999)
+          .toString()
+          .substring(0, 4);
+        //find list of codes
+        if (!_.contains(allcodes, newcode)) done = true;
+      }
+      //console.log('newcode: '+newcode);
+      cb(newcode);
+    });
+  },
 };
